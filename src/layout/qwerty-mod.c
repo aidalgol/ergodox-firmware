f0720f5f5058fd60fc9ee6ef4cb48796ae5d2171
Aidan Gauland <aidalgol@amuri.net>
2014-12-25 15:59:34 +1300

    Move aliases from layout source to key-functions
    Move the abbreviated macros for kbfun_ functions from the layout code to
    lib/key-functions/public.h so that they're defined in the same place as
    the functions themselves.

--- src/layout/qwerty-mod.c
+++ src/layout/qwerty-mod.c
@@ -101,49 +101,6 @@ _F12, _F6,     _F7,     _F8,     _F9,    _F10,        0,
 
 };
 
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-
-// aliases
-
-// basic
-#define  kprrel  &kbfun_press_release
-#define  ktog    &kbfun_toggle
-#define  ktrans  &kbfun_transparent
-// --- layer push/pop functions
-#define  lpush1   &kbfun_layer_push_1
-#define  lpush2   &kbfun_layer_push_2
-#define  lpush3   &kbfun_layer_push_3
-#define  lpush4   &kbfun_layer_push_4
-#define  lpush5   &kbfun_layer_push_5
-#define  lpush6   &kbfun_layer_push_6
-#define  lpush7   &kbfun_layer_push_7
-#define  lpush8   &kbfun_layer_push_8
-#define  lpush9   &kbfun_layer_push_9
-#define  lpush10  &kbfun_layer_push_10
-#define  lpop1    &kbfun_layer_pop_1
-#define  lpop2    &kbfun_layer_pop_2
-#define  lpop3    &kbfun_layer_pop_3
-#define  lpop4    &kbfun_layer_pop_4
-#define  lpop5    &kbfun_layer_pop_5
-#define  lpop6    &kbfun_layer_pop_6
-#define  lpop7    &kbfun_layer_pop_7
-#define  lpop8    &kbfun_layer_pop_8
-#define  lpop9    &kbfun_layer_pop_9
-#define  lpop10   &kbfun_layer_pop_10
-// ---
-
-// device
-#define  dbtldr  &kbfun_jump_to_bootloader
-
-// special
-#define  sshprre  &kbfun_shift_press_release
-#define  s2kcap   &kbfun_2_keys_capslock_press_release
-#define  slpunum  &kbfun_layer_push_numpad
-#define  slponum  &kbfun_layer_pop_numpad
-
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
 
 const void_funptr_t PROGMEM _kb_layout_press[KB_LAYERS][KB_ROWS][KB_COLUMNS] = {
 
--- src/lib/key-functions/public.h
+++ src/lib/key-functions/public.h
@@ -18,7 +18,7 @@ void kbfun_press_release(void);
 void kbfun_press_release_preserve_sticky(void);
 void kbfun_toggle(void);
 void kbfun_transparent(void);
-// --- layer push/pop functions
+// layer push/pop functions
 void kbfun_layer_push_1(void);
 void kbfun_layer_push_2(void);
 void kbfun_layer_push_3(void);
@@ -49,7 +49,6 @@ void kbfun_layer_pop_7(void);
 void kbfun_layer_pop_8(void);
 void kbfun_layer_pop_9(void);
 void kbfun_layer_pop_10(void);
-// ---
 
 // device
 void kbfun_jump_to_bootloader(void);
@@ -60,5 +59,41 @@ void kbfun_2_keys_capslock_press_release(void);
 void kbfun_layer_push_numpad(void);
 void kbfun_layer_pop_numpad(void);
 void kbfun_mediakey_press_release(void);
+
+/** aliases **/
+// basic
+#define  kprrel  &kbfun_press_release
+#define  ktog    &kbfun_toggle
+#define  ktrans  &kbfun_transparent
+// layer push/pop functions
+#define  lpush1   &kbfun_layer_push_1
+#define  lpush2   &kbfun_layer_push_2
+#define  lpush3   &kbfun_layer_push_3
+#define  lpush4   &kbfun_layer_push_4
+#define  lpush5   &kbfun_layer_push_5
+#define  lpush6   &kbfun_layer_push_6
+#define  lpush7   &kbfun_layer_push_7
+#define  lpush8   &kbfun_layer_push_8
+#define  lpush9   &kbfun_layer_push_9
+#define  lpush10  &kbfun_layer_push_10
+#define  lpop1    &kbfun_layer_pop_1
+#define  lpop2    &kbfun_layer_pop_2
+#define  lpop3    &kbfun_layer_pop_3
+#define  lpop4    &kbfun_layer_pop_4
+#define  lpop5    &kbfun_layer_pop_5
+#define  lpop6    &kbfun_layer_pop_6
+#define  lpop7    &kbfun_layer_pop_7
+#define  lpop8    &kbfun_layer_pop_8
+#define  lpop9    &kbfun_layer_pop_9
+#define  lpop10   &kbfun_layer_pop_10
+
+// device
+#define  dbtldr  &kbfun_jump_to_bootloader
+
+// special
+#define  sshprre  &kbfun_shift_press_release
+#define  s2kcap   &kbfun_2_keys_capslock_press_release
+#define  slpunum  &kbfun_layer_push_numpad
+#define  slponum  &kbfun_layer_pop_numpad
 
 #endif
